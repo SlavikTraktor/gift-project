ac7f83c6c52e3cff7021af2feba0fe0e4f819467
Sviatoslav Sobakin <slavasobb@gmail.com>
2022-08-01 05:11:28 +0400

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import fastifyMiddle from "@fastify/middie";
 import { setUpEnvs } from "./config";
 
 setUpEnvs();
@@ -7,10 +8,24 @@ const fastify = Fastify({
   logger: true,
 });
 
-// Declare a route
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
-});
+fastify.register(fastifyMiddle);
+
+fastify.register(
+  (fastify, options, done) => {
+    fastify.get("/", function (request, reply) {
+      reply.send({ hello: "world1" });
+
+    });
+
+    fastify.get("/lul", function (request, reply) {
+      reply.send({ hello: "world2" });
+
+      // reply.code(405).send();
+    });
+    done();
+  },
+  { prefix: "/kek" }
+);
 
 // Run the server!
 fastify.listen({ port: process.env.APP_PORT }, function (err, address) {
