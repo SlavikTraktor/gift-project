d0d1a7a86cb55debae8013d3889e7fee3e8e32ee
Sviatoslav Sobakin <slavasobb@gmail.com>
2022-08-15 03:21:12 +0400

    fix

--- v-gift/src/components/RedirectRoute/RedirectRoute.tsx
+++ v-gift/src/components/RedirectRoute/RedirectRoute.tsx
@@ -10,6 +10,5 @@ export const RedirectRoute = ({
   redirectRoute = Routes.LOGIN,
   shouldRedirect,
 }: PrivateRouteProps) => {
-  console.log(shouldRedirect, shouldRedirect ? <Navigate to={redirectRoute} /> : <Outlet />);
   return shouldRedirect ? <Navigate to={redirectRoute} /> : <Outlet />;
 };
