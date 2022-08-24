5d2fa447a04993cfd32ee33e74ac7466b7c6eef3
Sviatoslav Sobakin <slavasobb@gmail.com>
2022-08-24 04:21:50 +0400

    test page

--- v-gift/src/App.tsx
+++ v-gift/src/App.tsx
@@ -1,28 +1,35 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { ChoosePartner } from "./pages/ChoosePartner";
 import { useQuery } from "@tanstack/react-query";
 import * as ls from "local-storage";
 
-import { getPartner } from "./api/partner/getPartner";
-import { GETAUTH_QUERY, PARTNER_QUERY } from "./api/partner/partner";
+import { GETAUTH_QUERY } from "./api/partner/partner";
 import { SplitDesiresScreen } from "./pages/SplitDesiresScreen/SplitDesiresScreen";
 import { Route, Routes as ReactRouterRoutes } from "react-router-dom";
 import { RedirectRoute } from "@/components/RedirectRoute";
 import { Routes } from "./constants/routes";
-import { LoginPage } from "./pages/LoginPage";
+import { LoginPage } from "@/pages/LoginPage";
+import { TestPage } from "@/pages/TestPage";
 
 function App() {
   const token = useQuery([GETAUTH_QUERY], () => {
     return ls.get<string>("refreshToken");
   });
 
+  if (token.isLoading) {
+    return <></>;
+  }
 
   return (
     <div className="text-center">
       <ReactRouterRoutes>
-        <Route path="/" element={<RedirectRoute shouldRedirect={!token.data} />}>
+        <Route
+          path="/"
+          element={<RedirectRoute shouldRedirect={!token.data} />}
+        >
           <Route path={Routes.PARTNER} element={<ChoosePartner />} />
           <Route path={Routes.HOME} element={<SplitDesiresScreen />} />
+          <Route path={Routes.TEST} element={<TestPage />} />
         </Route>
         <Route
           path="/"
--- v-gift/src/constants/routes.ts
+++ v-gift/src/constants/routes.ts
@@ -2,4 +2,5 @@ export enum Routes {
   HOME = "/",
   LOGIN = "/login",
   PARTNER = "/partner",
+  TEST = "/test",
 }
