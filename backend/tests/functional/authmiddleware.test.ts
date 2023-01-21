f70c37863b1be20541203414321191ac170b6623
Sviatoslav Sobakin <slavasobb@gmail.com>
2023-01-22 02:41:55 +0400

    fix

--- backend/tests/functional/authmiddleware.test.ts
+++ backend/tests/functional/authmiddleware.test.ts
@@ -22,7 +22,7 @@ tap.test("Auth middleware", async (t) => {
       accessToken
     );
 
-    t.equal(response.statusCode, 201);
+    t.equal(response.statusCode, 200);
   });
 
   t.test("with wrong token returns 401", async (t) => {
