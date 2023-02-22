356d936ea4c7c0de3790f14a8cb7f58cfdbdf5d0
Vahushty <juarinio228@gmail.com>
2023-02-22 14:05:58 +0300

    first push

---
+++ backend/src/routes/register/regRoute.ts
@@ -0,0 +1,29 @@
+import {  regCredentials } from "@/services/regService";
+import { FastifyPluginCallback } from "fastify";
+import {
+  PostCredentialType,
+  PostCredentialValidation,
+} from "./types/postCredentials";
+
+export const regRoutes: FastifyPluginCallback = (fastify, options, done) => {
+  fastify.post<{
+    Body: PostCredentialType;
+  }>(
+    "/",
+    {
+      schema: {
+        body: PostCredentialValidation,
+      },
+    },
+    async (req, res) => {
+        try {
+            await regCredentials(req.body)
+        } catch (error){
+            res.code(403);
+            res.send(error);
+        }
+    //   res.send({ hello: "Sosizopu" });
+    },
+  );
+  done();
+};
