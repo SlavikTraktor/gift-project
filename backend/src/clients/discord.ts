7e123129fb0c1ca01a3ef4425443b639f44d432f
Vahushty <juarinio228@gmail.com>
2023-04-03 12:06:40 +0300

    Убрал краш сервака, при пустых ENV данных

--- backend/src/clients/discord.ts
+++ backend/src/clients/discord.ts
@@ -3,19 +3,16 @@ import DiscordOAuth2 from "discord-oauth2";
 const clientId = process.env.DISCORD_CLIENT_ID;
 if (!clientId) {
   console.error("DISCORD_CLIENT_ID is not defined");
-  process.exit(1);
 }
 
 const clientSecret = process.env.DISCORD_CLIENT_SECRET;
 if (!clientSecret) {
   console.error("DISCORD_CLIENT_SECRET is not defined");
-  process.exit(1);
 }
 
 const redirectUri = process.env.DISCORD_REDIRECT_URI;
 if (!redirectUri) {
   console.error("DISCORD_REDIRECT_URI is not defined");
-  process.exit(1);
 }
 
 export const discordOauth2Client = () =>
--- backend/src/clients/google.ts
+++ backend/src/clients/google.ts
@@ -3,19 +3,16 @@ import { OAuth2Client } from "google-auth-library";
 const clientId = process.env.GOOGLE_CLIENT_ID;
 if (!clientId) {
   console.error("GOOGLE_CLIENT_ID is not defined");
-  process.exit(1);
 }
 
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 if (!clientSecret) {
   console.error("GOOGLE_CLIENT_SECRET is not defined");
-  process.exit(1);
 }
 
 const redirectUri = process.env.GOOGLE_REDIRECT_URI;
 if (!redirectUri) {
   console.error("GOOGLE_REDIRECT_URI is not defined");
-  process.exit(1);
 }
 
 export const googleOauth2Client = () =>
