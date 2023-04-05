4d45a24e8814398eaa0d843e11b21747cc652150
Vahushty <juarinio228@gmail.com>
2023-04-05 12:33:40 +0300

    Исправлена строчка при валидации пароля

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -19,8 +19,10 @@ export const loginUser = async (name: string, pass: string) => {
   if (user.discordReg == true) {
     throw new Error("Login through Discord");
   }
-
-  const validatePassword = await bcrypt.compare(pass, user.password as string);
+  if (user.password == null) {
+    throw new Error("Invalid pasword");
+  }
+  const validatePassword = await bcrypt.compare(pass, user.password);
   if (!validatePassword) {
     throw new Error("Invalid password");
   }
