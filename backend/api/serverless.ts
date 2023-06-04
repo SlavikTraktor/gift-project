a9549e2e500cc885f485b8660a9727beb48fa026
Slava Sobakin <slavasobb@gmail.com>
2023-06-04 06:16:27 +0400

    test

--- backend/api/serverless.ts
+++ backend/api/serverless.ts
@@ -14,9 +14,9 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../dist/src/app.js"));
+app.register(import("../src/app") as any);
 
-export default async (req, res) => {
+export default async (req: any, res: any) => {
   await app.ready();
   app.server.emit("request", req, res);
 };
