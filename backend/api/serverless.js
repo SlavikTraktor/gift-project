84b0e78b52f9ed0cfab498cf09a4ade547aa68f4
Slava Sobakin <slavasobb@gmail.com>
2023-06-04 06:46:06 +0400

    change

--- backend/api/serverless.js
+++ backend/api/serverless.js
@@ -5,18 +5,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// Require the framework
-import Fastify from "fastify";
-
-import appInternal from "../dist/src/app.js";
-
-// Instantiate Fastify with some config
-const app = Fastify({
-  logger: true,
-});
-
-// Register your application as a normal plugin.
-app.register(appInternal);
+import app from "../dist/src/app.js";
 
 export default async (req, res) => {
   await app.ready();
